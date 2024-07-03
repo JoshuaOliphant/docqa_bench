bf3eec3a3d1aa1bb2ad700fa85b0a4da1b3cf561
Joshua Oliphant <joshua.oliphant@hey.com>
2024-07-03 20:20:25 +0000

    Added abstract count method to base vector store

--- docqa_bench/core/vector_store.py
+++ docqa_bench/core/vector_store.py
@@ -1,11 +1,17 @@
 from abc import ABC, abstractmethod
 from typing import List, Dict
 
+
 class BaseVectorStore(ABC):
+
     @abstractmethod
     async def add(self, id: str, vector: List[float], metadata: dict):
         pass
 
     @abstractmethod
     async def search(self, query_vector: List[float], k: int) -> List[Dict]:
+        pass
+
+    @abstractmethod
+    async def count(self) -> int:
         pass
